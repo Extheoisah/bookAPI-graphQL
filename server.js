d5ebb4c8c629a0a02af733a010d02a89afcd6447
Theophilus <isahtheophilus@gmail.com>
2022-05-28 23:18:39 +0100

    initialized code base and installed project dependencies

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const app = express();
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: "HelloWorld",
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => "Hello world"
+            }
+        })
+    })
+})
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+      schema: schema,
+    graphiql: true,
+  })
+);
+app.listen(5000, () => console.log("Server running"));
